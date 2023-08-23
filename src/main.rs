cd2a056dd5631a8396663b02bd83f520e25b8770
Mr Speaker <mrspeaker@gmail.com>
2023-08-23 20:31:17 +0800

    playa

--- src/main.rs
+++ src/main.rs
@@ -10,20 +10,42 @@ struct Dir {
 #[derive(Component)]
 struct Bob;
 
+#[derive(Component)]
+struct Playa;
+
 fn main() {
     App::new()
         .insert_resource(ClearColor(Color::rgb(0.9, 0.9, 0.9)))
         .add_plugins(DefaultPlugins)
         .add_systems(Startup, setup)
-        .add_systems(Update, (sys_move, sys_bobbin))
+        .add_systems(Update, (sys_move, sys_bobbin, sys_keys))
         .run();
+}
+
+fn sys_keys(key_in: Res<Input<KeyCode>>,
+            time: Res<Time>,
+            mut query: Query<&mut Transform, With<Playa>>) {
+    let mut transform = query.single_mut();
+    let speed = 300.0;
+    if key_in.pressed(KeyCode::Right) {
+        transform.translation.x += speed * time.delta_seconds();
+    }
+    if key_in.pressed(KeyCode::Left) {
+        transform.translation.x -= speed * time.delta_seconds();
+    }
+    if key_in.pressed(KeyCode::Up) {
+        transform.translation.y += speed * time.delta_seconds();
+    }
+    if key_in.pressed(KeyCode::Down) {
+        transform.translation.y -= speed * time.delta_seconds();
+    }
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     let mut rng = rand::thread_rng();
     commands.spawn(Camera2dBundle::default());
 
-    for _i in 0..1000 {
+    for _i in 0..10 {
         commands.spawn((
             SpriteBundle {
                 texture: asset_server.load("char.png"),
@@ -44,6 +66,22 @@ fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
             }
         ));
     }
+
+    commands.spawn((
+        SpriteBundle {
+            texture: asset_server.load("monsta.png"),
+            transform: Transform::from_xyz(
+                (rng.gen::<f32>()) * 1000. - 500.,
+                (rng.gen::<f32>()) * 500. - 250.,
+                0.)
+                .with_scale(Vec3::splat(0.25)),
+            sprite: Sprite {
+                flip_y: false,
+                ..default()
+            },
+            ..default()
+        },
+        Playa));
 }
 
 fn sys_move(mut commands: Commands, time: Res<Time>, mut pos: Query<(Entity, &mut Transform, &mut Dir, &mut Sprite)>) {
